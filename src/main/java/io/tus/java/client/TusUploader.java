4f274d607530eaa5d570995d939052b3984d5266
Mac <mac@Macs-MacBook-Pro.local>
2021-01-06 18:31:27 +0500

    ..

--- src/main/java/io/tus/java/client/TusUploader.java
+++ src/main/java/io/tus/java/client/TusUploader.java
@@ -237,7 +237,7 @@ public class TusUploader {
         Cipher cipher = null;
         cipher = Cipher.getInstance("AES_256/GCM/NoPadding");
         try {
-            GCMParameterSpec spec = new GCMParameterSpec(128,new IvParameterSpec("1234567890123456".getBytes(StandardCharsets.UTF_8)).getIV());
+            GCMParameterSpec spec = new GCMParameterSpec(256,new IvParameterSpec("1234567890123456".getBytes(StandardCharsets.UTF_8)).getIV());
             cipher.init(Cipher.ENCRYPT_MODE, secret, spec);
         } catch (InvalidAlgorithmParameterException e) {
             e.printStackTrace();
